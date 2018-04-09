72c3c1781123a8c67d1543343b54e301c2e53e6e
chucongran <chucongran@LVZHOUKF07.h3c.huawei-3com.com>
2018-04-09 19:52:24 +0800

    22

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ app.use('/act', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  // 
+  res.end("404")
 });
 
 // error handler
@@ -32,7 +33,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  // res.render('error');
 });
 
 module.exports = app;
