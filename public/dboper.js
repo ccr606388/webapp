8908f7709b563094f2b0df02a5d5b73dce2f3e2e
chucongran <chucongran@LVZHOUKF07.h3c.huawei-3com.com>
2018-04-09 15:44:33 +0800

    添加数据库操作以及测试代码  BY CC

---
+++ public/dboper.js
@@ -0,0 +1,45 @@
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var dbhandle = null;
+// Connection URL
+var url = 'mongodb://101.200.40.203:27017';
+
+// Database Name
+var dbName = 'activity';
+
+// Use connect method to connect to the server
+function dbInit(mongoName)
+{
+    MongoClient.connect(url, function(err, client) {
+        assert.equal(null, err);
+        console.log("Connected successfully to server");
+        dbhandle = client.db(mongoName || dbName);
+    });
+}
+
+function dbInsert(templateName, data, callback)
+{
+    var collection = dbhandle.collection(templateName);
+
+    collection.insertOne(data, callback);
+}
+
+function dbFind(templateName, query, callback)
+{
+    var collection = dbhandle.collection(templateName);
+
+    var temp = collection.find(query).toArray();
+    callback(temp);
+}
+
+function dbClear(templateName, callback)
+{
+    dbhandle.dropCollection(templateName, callback);
+}
+
+module.exports = {
+    dbInit : dbInit,
+    dbInsert : dbInsert,
+    dbFind : dbFind,
+    dbClear : dbClear
+};
---
+++ test/dbtest.js
@@ -0,0 +1,41 @@
+var mongodb = require("../public/dboper");
+
+mongodb.dbInit("test");
+
+setTimeout(function(){
+
+    mongodb.dbInsert("test_collection", {a:1,b:2}, function(err){
+        console.log("insert data1");
+        console.log(err);
+    })
+    
+    mongodb.dbInsert("test_collection", {a:2,b:3}, function(err){
+        console.log("insert data2");
+        console.log(err);
+    })
+    
+    mongodb.dbFind("test_collection", {a:1}, function(data){
+        console.log("find data1");
+        console.log(data);
+    })
+    
+    mongodb.dbFind("test_collection", {}, function(data){
+        console.log("find data2");
+        console.log(data);
+    })
+    
+    mongodb.dbFind("test_collection", {a:3}, function(data){
+        console.log("find data3");
+        console.log(data);
+    })
+    
+    mongodb.dbClear("test_collection", function(err){
+        console.log("clear data1");
+    })
+    mongodb.dbFind("test_collection", {}, function(data){
+        console.log("clear data1");
+        console.log(data);
+    })
+
+}, 10000)
+
