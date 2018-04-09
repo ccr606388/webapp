110bddb8890766df73686487e27c72117aa0ac08
ccr606388 <294385049@qq.com>
2018-04-09 21:37:13 +0800

    11

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ router.use('/cfgtemplate', function(req, res, next){
     }
     var temId = req.query.pageid || req.body.pageid;
     // console.log("access cfgtemplate");
-    dboper.dbUpsert(TEMPLATE_CFG, {pageid : pageid}, conf, function(err){
+    dboper.dbUpsert(TEMPLATE_CFG, {pageid : temId}, conf, function(err){
         res.json({
             result: err?1:0,
             reason: err?"failed with err: "+JSON.stringify(err):"success"
@@ -25,7 +25,7 @@ router.use('/cfgtemplate', function(req, res, next){
 router.get('/gettemplate', function(req, res, next){
     console.log("access gettemplate");
     var temId = req.query.pageid || req.body.pageid;
-    dboper.dbFind(TEMPLATE_CFG, {pageid : pageid}, function(data){
+    dboper.dbFind(TEMPLATE_CFG, {pageid : temId}, function(data){
         console.log("find finish ",data)
         res.json({
             result: 0,
